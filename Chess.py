3971ad8455085c29479cf454d23bc56f41131c36
LeoASH1 <leo.ashworth.la@gmail.com>
2025-10-04 21:18:12 +0100

    Added board and pieces

--- Chess.py
+++ Chess.py
@@ -1,4 +1,88 @@
 #responsible for user input and showing current game state
 
-import pygame
-pygame.init()
+import pygame as p
+p.init()
+import ChessEngine
+
+WIDTH = HEIGHT = 400
+DIMENSION = 8 #8x8 board could be !!CHANGED!! for multiple player chess/ new gamemode
+SquareSize = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+#initiliase global dictionary of images, called once 
+#can !!CHANGE!! it so player can change skins 
+def loadImages():
+    pieces = ["wp","wR", "wN", "wB", "wQ", "wK",  "bp", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        # transform to make sure peices are of correct and same size
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SquareSize, SquareSize))
+
+
+# main driver that handles user input and updates graphics 
+def main():
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    #drawBoard(screen)
+    #screen.fill((255, 255, 255))
+    #GameState variable that calls ChessEngine with its variables included 
+    gs = ChessEngine.GameState()
+    loadImages()
+    drawGameState(screen, gs)
+    running = True
+    while running:
+        #lists events since last frame
+        for e in p.event.get():
+        #if player trys to close application, end it
+            if e.type == p.QUIT:
+                running = False
+
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+#draws the graphics 
+def drawGameState(screen, gs):
+    drawBoard(screen)#draws squares onto board
+    #add piece !!HIGHLIGHTER!!
+    drawPieces(screen, gs.board)#draws pieces onto squares
+
+
+def drawBoard(screen):
+    
+    XCoordinate = 0
+    YCoordinate = 0
+
+    for row in range(8):
+        for column in range(8):
+            if (column + row) % 2 == 0:
+                p.draw.rect(screen, (118, 150, 86), (XCoordinate, YCoordinate, SquareSize, SquareSize))
+            else:
+                p.draw.rect(screen, (238, 238, 210), (XCoordinate, YCoordinate, SquareSize, SquareSize))
+            XCoordinate += SquareSize
+        YCoordinate += SquareSize
+        XCoordinate = 0
+
+
+
+def drawPieces(screen, board):
+
+    XCoordinate = 0
+    YCoordinate = 0
+
+    for row in range(8):
+        for column in range(8):
+                piece = board[row][column]
+                if piece != "--":
+                    screen.blit(IMAGES[piece], p.Rect((XCoordinate, YCoordinate, SquareSize, SquareSize)))
+                XCoordinate += SquareSize
+
+        YCoordinate += SquareSize
+        XCoordinate = 0
+
+
+            
+
+#means code is only ran when file is executed directly
+if __name__ == "__main__":
+    main()
--- ChessEngine.py
+++ ChessEngine.py
@@ -1,10 +1,11 @@
 #stores information about current state of chess game
 #determines valid moves at given state, keeping track of moves made
 class GameState():
-    def _init_(self):
+    def __init__(self):
         self.board = [
         #can use numpy arrays for IMPROVEMENT
-        # using -- instead of 0 as we can treat it the exact same way as other strings for ease of use
+        # using -- instead of 0 for empty space as we can treat it the exact same way as other strings for ease of use
+        #2D board where first character is colour and second is piece name
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
@@ -14,3 +15,5 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
         ]
+        self.whiteToMove = True
+        self.moveLog = []
